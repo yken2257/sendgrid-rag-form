658bfa1abd5f15867a38a5ac46babb7a334be530
kento-yoshida <kento-yoshida@kke.co.jp>
2024-12-22 23:56:52 +0000

    eslintのエラーを修正

--- components/memorized-markdown.tsx
+++ components/memorized-markdown.tsx
@@ -1,71 +1,102 @@
-import { marked } from 'marked';
-import { memo, useMemo } from 'react';
-import ReactMarkdown from 'react-markdown';
+import { marked } from "marked";
+import { memo, useMemo } from "react";
+import ReactMarkdown from "react-markdown";
 
 function parseMarkdownIntoBlocks(markdown: string): string[] {
-  const tokens = marked.lexer(markdown);
-  return tokens.map(token => token.raw);
+	const tokens = marked.lexer(markdown);
+	return tokens.map((token) => token.raw);
 }
 
-const CustomLink = ({ children, href, ...props }: React.PropsWithChildren<React.AnchorHTMLAttributes<HTMLAnchorElement>>) => {
-  return (
-    <a className='text-blue-600' href={href} target='_blank' rel='noopener noreferrer'>{children}</a>
-  );
+const CustomLink = ({
+	children,
+	href,
+}: React.PropsWithChildren<React.AnchorHTMLAttributes<HTMLAnchorElement>>) => {
+	return (
+		<a
+			className="text-blue-600"
+			href={href}
+			target="_blank"
+			rel="noopener noreferrer"
+		>
+			{children}
+		</a>
+	);
 };
 
-const CustomBulletList = ({ children }: React.PropsWithChildren<{}>) => {
-  return (
-    <ul className='list-disc list-inside'>{children}</ul>
-  );
-}
+const CustomBulletList = ({
+	children,
+	...props
+}: React.PropsWithChildren<React.HTMLAttributes<HTMLUListElement>>) => {
+	return (
+		<ul className="list-disc list-inside" {...props}>
+			{children}
+		</ul>
+	);
+};
 
-const CustomNumberedList = ({ children }: React.PropsWithChildren<{}>) => {
-  return (
-    <ol className='list-decimal list-inside'>{children}</ol>
-  );
-}
+const CustomNumberedList = ({
+	children,
+	...props
+}: React.PropsWithChildren<React.HTMLAttributes<HTMLOListElement>>) => {
+	return (
+		<ol className="list-decimal list-inside" {...props}>
+			{children}
+		</ol>
+	);
+};
 
-const CustomCodeBlock = ({ children }: React.PropsWithChildren<{}>) => {
-  return (
-    <pre className='bg-gray-100 p-4 rounded-md'>{children}</pre>
-  );
-}
+const CustomCodeBlock = ({
+	children,
+	...props
+}: React.PropsWithChildren<React.HTMLAttributes<HTMLElement>>) => {
+	return (
+		<pre className="bg-gray-100 p-4 rounded-md" {...props}>
+			{children}
+		</pre>
+	);
+};
 
-const CustomInlineCode = ({ children }: React.PropsWithChildren<{}>) => {
-  return (
-    <code className='bg-gray-100 p-1 rounded-md'>{children}</code>
-  );
-}
-
+const CustomInlineCode = ({
+	children,
+	...props
+}: React.PropsWithChildren<React.HTMLAttributes<HTMLElement>>) => {
+	return (
+		<code className="bg-gray-100 p-1 rounded-md" {...props}>
+			{children}
+		</code>
+	);
+};
 
 const MemoizedMarkdownBlock = memo(
-  ({ content }: { content: string }) => {
-    return <ReactMarkdown
-      components={{
-        a: CustomLink,
-        ul: CustomBulletList,
-        ol: CustomNumberedList,
-        pre: CustomCodeBlock,
-        code: CustomInlineCode,
-      }}
-    >{content}</ReactMarkdown>;
-  },
-  (prevProps, nextProps) => {
-    if (prevProps.content !== nextProps.content) return false;
-    return true;
-  },
+	({ content }: { content: string }) => {
+		return (
+			<ReactMarkdown
+				components={{
+					a: CustomLink,
+					ul: CustomBulletList,
+					ol: CustomNumberedList,
+					pre: CustomCodeBlock,
+					code: CustomInlineCode,
+				}}
+			>
+				{content}
+			</ReactMarkdown>
+		);
+	},
+	(prevProps, nextProps) => {
+		if (prevProps.content !== nextProps.content) return false;
+		return true;
+	},
 );
 
-MemoizedMarkdownBlock.displayName = 'MemoizedMarkdownBlock';
+MemoizedMarkdownBlock.displayName = "MemoizedMarkdownBlock";
 
-export const MemoizedMarkdown = memo(
-  ({ content }: { content: string; }) => {
-    const blocks = useMemo(() => parseMarkdownIntoBlocks(content), [content]);
+export const MemoizedMarkdown = memo(({ content }: { content: string }) => {
+	const blocks = useMemo(() => parseMarkdownIntoBlocks(content), [content]);
 
-    return blocks.map((block, index) => (
-      <MemoizedMarkdownBlock content={block} key={`block_${index}`} />
-    ));
-  },
-);
+	return blocks.map((block, index) => (
+		<MemoizedMarkdownBlock content={block} key={`block_${index}`} />
+	));
+});
 
-MemoizedMarkdown.displayName = 'MemoizedMarkdown';
+MemoizedMarkdown.displayName = "MemoizedMarkdown";
